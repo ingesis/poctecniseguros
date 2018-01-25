6f0d41c32b5181c67daf63ac831b107d4258f7e3
Alejandro Chinchilla Ayapán <alejandro.chinchilla@ingesis.com>
2018-01-25 16:00:50 +0000

    Merge branch 'master' of https://github.com/ingesis/poctecniseguros.git

---
+++ webapp/controller/HomePage.controller.js
@@ -0,0 +1,48 @@
+sap.ui.define(["sap/ui/core/mvc/Controller",
+	"sap/m/MessageBox",
+	"./utilities",
+	"sap/ui/core/routing/History"
+], function(BaseController, MessageBox, Utilities, History) {
+	"use strict";
+
+	return BaseController.extend("com.sap.build.standard.bancasegurosEmision.controller.HomePage", {
+		handleRouteMatched: function(oEvent) {
+
+			var oParams = {};
+
+			if (oEvent.mParameters.data.context) {
+				this.sContext = oEvent.mParameters.data.context;
+				var oPath;
+				if (this.sContext) {
+					oPath = {
+						path: "/" + this.sContext,
+						parameters: oParams
+					};
+					this.getView().bindObject(oPath);
+				}
+			}
+
+		},
+		onInit: function() {
+
+			this.mBindingOptions = {};
+			this.oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+			this.oRouter.getTarget("HomePage").attachDisplay(jQuery.proxy(this.handleRouteMatched, this));
+			var oView = this.getView();
+			oView.addEventDelegate({
+				onBeforeShow: function() {
+					if (sap.ui.Device.system.phone) {
+						var oPage = oView.getContent()[0];
+						if (oPage.getShowNavButton && !oPage.getShowNavButton()) {
+							oPage.setShowNavButton(true);
+							oPage.attachNavButtonPress(function() {
+								this.oRouter.navTo("MenuBar", {}, true);
+							}.bind(this));
+						}
+					}
+				}.bind(this)
+			});
+
+		}
+	});
+}, /* bExport= */ true);
